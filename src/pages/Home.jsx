05128bb0582588e97d2591797a51edf2748cbc15
SDG-projects <forliveprojectgit@gmail.com>
2024-12-26 09:48:25 +0530

    add  products components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
+import Products from "../components/Products";
 
 function Home() {
   return (
     <div>
       <HeroSection />
+      <Products />
     </div>
   );
 }
