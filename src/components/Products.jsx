db18c1067b6219fea5e0aa5dea46bcd17fe7d7ca
unknown <forliveprojectgit@gmail.com>
2024-12-30 17:14:20 +0530

    pdsection

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import "./styles/products.css";
  // Include styles for this component
