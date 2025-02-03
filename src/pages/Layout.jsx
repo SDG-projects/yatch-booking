32ebea5083066cb6bc5e89ef144238dc28bf100a
SDG-projects <forliveprojectgit@gmail.com>
2025-02-03 17:42:01 +0530

    .

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "./style.css";
@@ -41,6 +41,10 @@ function ScrollUP() {
   );
 }
 function Layout() {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
   return (
     <div className="layout">
       <Navbar />
