8aeb0eaa0a81eb512894f24c897bddadc977f307
SDG-projects <forliveprojectgit@gmail.com>
2025-03-25 07:13:49 +0530

    seprate the service component

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -25,12 +25,67 @@ export const serviceWhatsAppRedirect = (service) => {
 
   window.open(whatsappUrl, "_blank");
 };
+function Service({ serviceData }) {
+  return (
+    <Suspense fallback={<Loading />}>
+      <div className="serviceCon">
+        <div className="serviceContent">
+          <div className="serviceImgCon">
+            {serviceData.img && (
+              // <Suspense fallback={<Loading />}>
+              <img
+                src={serviceData.img}
+                className="serviceImg"
+                alt={serviceData.name}
+                loading="lazy"
+              />
+              // </Suspense>
+            )}
+            {serviceData.img2 && (
+              // <Suspense fallback={<Loading />}>
+              <img
+                src={serviceData.img2}
+                className="serviceImg"
+                alt={serviceData.name}
+                loading="lazy"
+              />
+              // </Suspense>
+            )}
+          </div>
+          <div className="serviceDet">
+            <h2>{serviceData.name}</h2>
+            <h3>{serviceData.heading || "No Heading Available"}</h3>
+            <div
+              dangerouslySetInnerHTML={{
+                __html:
+                  serviceData.description?.replace(/\n/g, "<br />") ||
+                  "No Description",
+              }}
+            />
+            <h3>We Offer:</h3>
+            <p>{serviceData.weoffer1 || "Not Available"}</p>
+            <p>{serviceData.weoffer2 || "Not Available"}</p>
+            <p>{serviceData.weoffer3 || "Not Available"}</p>
+            <p>{serviceData.weoffer4 || "Not Available"}</p>
+            <h3>Rating: {serviceData.rating || "No Rating"}</h3>
+            <button
+              className="bookNowBtn"
+              onClick={() => serviceWhatsAppRedirect(serviceData)}
+            >
+              Book Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </Suspense>
+  );
+}
 
 function Services() {
   const { services } = useContext(DataContext);
   const [loading, setLoading] = useState(true);
   const { service } = useParams();
-  const serviceData = services.filter((serv) => serv.name == service)[0];
+  const serviceData = services.find((serv) => serv.name == service);
   console.log(serviceData);
   // ✅ Extract service name without "&2" or extra parts
   // const cleanServiceName = service?.split("&")[0].trim();
@@ -74,61 +129,65 @@ function Services() {
       <h1 className="service-h1">Our Services</h1>
       <p className="service-p">Explore the exclusive services we offer</p>
       <hr className="styled-line" />
-
-      {serviceData ? (
-        <Suspense fallback={<Loading />}>
-          <div className="serviceCon">
-            <div className="serviceContent">
-              <div className="serviceImgCon">
-                {serviceData.img && (
-                  // <Suspense fallback={<Loading />}>
-                  <img
-                    src={serviceData.img}
-                    className="serviceImg"
-                    alt={serviceData.name}
-                    loading="lazy"
-                  />
-                  // </Suspense>
-                )}
-                {serviceData.img2 && (
-                  // <Suspense fallback={<Loading />}>
-                  <img
-                    src={serviceData.img2}
-                    className="serviceImg"
-                    alt={serviceData.name}
-                    loading="lazy"
-                  />
-                  // </Suspense>
-                )}
-              </div>
-              <div className="serviceDet">
-                <h2>{serviceData.name}</h2>
-                <h3>{serviceData.heading || "No Heading Available"}</h3>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html:
-                      serviceData.description?.replace(/\n/g, "<br />") ||
-                      "No Description",
-                  }}
-                />
-                <h3>We Offer:</h3>
-                <p>{serviceData.weoffer1 || "Not Available"}</p>
-                <p>{serviceData.weoffer2 || "Not Available"}</p>
-                <p>{serviceData.weoffer3 || "Not Available"}</p>
-                <p>{serviceData.weoffer4 || "Not Available"}</p>
-                <h3>Rating: {serviceData.rating || "No Rating"}</h3>
-                <button
-                  className="bookNowBtn"
-                  onClick={() => serviceWhatsAppRedirect(serviceData)}
-                >
-                  Book Now
-                </button>
-              </div>
-            </div>
-          </div>
-        </Suspense>
+      {service ? (
+        serviceData ? (
+          // <Suspense fallback={<Loading />}>
+          //   <div className="serviceCon">
+          //     <div className="serviceContent">
+          //       <div className="serviceImgCon">
+          //         {serviceData.img && (
+          //           // <Suspense fallback={<Loading />}>
+          //           <img
+          //             src={serviceData.img}
+          //             className="serviceImg"
+          //             alt={serviceData.name}
+          //             loading="lazy"
+          //           />
+          //           // </Suspense>
+          //         )}
+          //         {serviceData.img2 && (
+          //           // <Suspense fallback={<Loading />}>
+          //           <img
+          //             src={serviceData.img2}
+          //             className="serviceImg"
+          //             alt={serviceData.name}
+          //             loading="lazy"
+          //           />
+          //           // </Suspense>
+          //         )}
+          //       </div>
+          //       <div className="serviceDet">
+          //         <h2>{serviceData.name}</h2>
+          //         <h3>{serviceData.heading || "No Heading Available"}</h3>
+          //         <div
+          //           dangerouslySetInnerHTML={{
+          //             __html:
+          //               serviceData.description?.replace(/\n/g, "<br />") ||
+          //               "No Description",
+          //           }}
+          //         />
+          //         <h3>We Offer:</h3>
+          //         <p>{serviceData.weoffer1 || "Not Available"}</p>
+          //         <p>{serviceData.weoffer2 || "Not Available"}</p>
+          //         <p>{serviceData.weoffer3 || "Not Available"}</p>
+          //         <p>{serviceData.weoffer4 || "Not Available"}</p>
+          //         <h3>Rating: {serviceData.rating || "No Rating"}</h3>
+          //         <button
+          //           className="bookNowBtn"
+          //           onClick={() => serviceWhatsAppRedirect(serviceData)}
+          //         >
+          //           Book Now
+          //         </button>
+          //       </div>
+          //     </div>
+          //   </div>
+          // </Suspense>
+          <Service serviceData={serviceData} />
+        ) : (
+          <p style={{ color: "red", fontSize: "20px" }}>Service not found</p>
+        )
       ) : (
-        <p style={{ color: "red", fontSize: "20px" }}>Service not found</p>
+        services.map((service, i) => <Service key={i} serviceData={service} />)
       )}
     </div>
   );
